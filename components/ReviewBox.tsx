d192981f7c4ad3f234f5212a9b4eab00178f1181
ian <ian@ians-MacBook-Pro.local>
2025-04-03 00:07:30 -0700

    Added More Elements to ReviewBox

--- components/ReviewBox.tsx
+++ components/ReviewBox.tsx
@@ -23,12 +23,20 @@ export default function ({name, content, uri} : Props){
 
     return(
         <View style={[{marginTop:35}]}>
+
             <View style={styles.rowContainer}>
                 <Image source={uri} style={styles.profile}/>
-                <Text style = {styles.header}>{name}</Text>
+                <View style = {styles.colContainer}>
+                  <Text style = {styles.header}>{name}</Text>
+                  <Text style={styles.desc}>App User • 26 Reviews • 5 photos</Text>
+                </View>
             </View>
-            <View style={[{marginLeft:24}, {marginTop:4}]}>
-                <ReviewStars s={15}/>
+
+            <View style={[styles.rowContainer, {marginLeft:24}, {marginTop:5}]}>
+                <View style={[{marginTop:2}]}>
+                  <ReviewStars s={13}/>
+                </View>
+                <Text style={[styles.desc, {marginTop:1}]}>1 Week ago</Text>
             </View>
             <View>
                 <Text style={styles.body}>{content}</Text>
@@ -47,6 +55,17 @@ const styles = StyleSheet.create({
       borderWidth: 1,
       borderRadius: 50,
     },
+    desc: {
+      marginTop: 2,
+      marginLeft: 10,
+      color: '#8D5179',
+      fontSize: 13,
+      fontFamily: 'Roboto-Medium',
+      width: 200,
+      height: 20,
+      borderColor : 'red',
+      borderWidth: 1,
+    },
     rowContainer: {
       flexDirection: 'row',
       borderWidth: 1,
@@ -56,21 +75,21 @@ const styles = StyleSheet.create({
       flexDirection: 'column',
     },
     body: {
-      marginTop: 10,
+      marginTop: 6,
       marginLeft: 24,
-      color: '444C51',
+      color: '#8D5179',
       fontSize: 14,
       fontFamily: 'Roboto-Medium',
       fontWeight: 'medium',
-      opacity: 0.7,
+      opacity: 0.8,
       width: 325,
       height: 200,
       borderColor : 'red',
       borderWidth: 1,
     },
     header: {
-        marginTop: 10,
-        marginLeft: 20,
+        marginTop: 0,
+        marginLeft: 10,
         color: '#7C3763',
         fontSize: 17,
         fontFamily: 'Roboto-Bold',
