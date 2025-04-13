f4c6c25ee3a1ebd194cf49cc2dad5fcb21ccf314
celestinebuendia <celestinembuendia@yahoo.com>
2025-04-13 03:37:13 -0700

    Weather refresh and option selection

--- components/WeatherView.tsx
+++ components/WeatherView.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import * as Location from 'expo-location';
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Switch, Text, TouchableOpacity, View } from "react-native";
 import { Feather } from '@expo/vector-icons'
 import { Ionicons } from '@expo/vector-icons'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { Foundation } from "@expo/vector-icons";
+import { SimpleLineIcons } from "@expo/vector-icons";
 import colors from "@/app/colors";
 
 type Alert = {
@@ -39,17 +40,23 @@ export default function WeatherView() {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
   const [alerts, setAlerts] = useState<Alert[]>([]);
   const [weather, setWeather] = useState<Weather | null>(null);
+  const [isAmer, setIsAmer] = useState<boolean>(true);
+  const [refresh, setRefresh] = useState<boolean>(false);
 
   useEffect(() => {
+    console.log("getting location");
     getCurrentLocationAysnc();
-  }, []);
+  }, [refresh]);
 
   useEffect(() => {
+    console.log("getting weather");
     if (location) {
       getWeatherAsync();
       getWeatherAlertsAsync();
     }
-  }, [location]);
+  }, [location, refresh]);
+
+  const toggleSwitch = () => setIsAmer(previousState => !previousState);
 
   const getCurrentLocationAysnc = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
@@ -101,8 +108,24 @@ export default function WeatherView() {
         </View>
       ) : (
         <View style={styles.weatherHeader}>
-          <WeatherIcon iconPath={weather?.condition.icon} altText={weather?.condition.text} />
-          <WeatherInfo isAmer={true} weather={weather} />
+          <View style={styles.weatherTop}>
+          <Pressable onPress={() => setRefresh(prev => !prev)}><SimpleLineIcons name="refresh" style={styles.weatherRefresh}/></Pressable>
+            <View style={styles.weatherSwitch}>
+              <Text style={styles.switchLabel}>°F</Text>
+              <Switch 
+                trackColor={{false: '#A9A9A9', true: '#A9A9A9'}}
+                thumbColor='#f3f3f3'
+                ios_backgroundColor='#A9A9A9'
+                onValueChange={toggleSwitch}
+                value={isAmer}
+              />
+              <Text style={styles.switchLabel}>°C</Text>
+            </View>
+          </View>
+          <View style={styles.weatherContent}>
+            <WeatherIcon iconPath={weather?.condition.icon} altText={weather?.condition.text} />
+            <WeatherInfo isAmer={!isAmer} weather={weather} />
+          </View>
         </View>
       )}
       <View style={styles.alertsContainer}>
@@ -164,11 +187,11 @@ function WeatherInfo({ isAmer, weather }: WeatherInfoProps) {
         <Text style={styles.infoSmall}>{weather.uv} UV  <MaterialCommunityIcons name="sun-wireless-outline" /></Text>
       </View>
     ) : (
-      <View>
-        <Text>{weather.temp_c} °C</Text>
-        <Text>{weather.wind_kph} kph {weather.wind_dir}</Text>
-        <Text>{weather.precip_mm} mm</Text>
-        <Text>{weather.uv} UV</Text>
+      <View style={styles.weatherInfo}>
+        <Text style={styles.infoTemp}>{weather.temp_c} °C</Text>
+        <Text style={styles.infoSmall}>{weather.wind_kph} kph  <Feather name="wind" /></Text>
+        <Text style={styles.infoSmall}>{weather.precip_mm} mm  <Ionicons name="rainy" /></Text>
+        <Text style={styles.infoSmall}>{weather.uv} UV  <MaterialCommunityIcons name="sun-wireless-outline" /></Text>
       </View>
     )
   )
@@ -187,10 +210,44 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     color: 'white',
     borderRadius: 10,
-    padding: 25,
+  },
+  weatherTop: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    paddingRight: 5,
+    marginBottom: 5
+  },
+  weatherRefresh: {
+    color: 'white',
+    fontSize: 20,
+    paddingTop: 15,
+    paddingLeft: 15,
+    opacity: 0.5,
+  },
+  weatherSwitch: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    transform: [
+      {scale: 0.7}
+    ],
+    marginTop: 10
+  },
+  switchLabel: {
+    color: 'white',
+    padding: 5,
+    fontSize: 18,
+    opacity: 0.5,
+  },
+  weatherContent: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 25,
+    paddingBottom: 25,
   },
   headerText: {
     color: 'white',
